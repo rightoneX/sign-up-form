1586999eb2e31aacc6ac78000e5a87441be4d2b0
rightoneX <alex@auco.co.nz>
2024-03-26 21:16:50 +1300

    replace css with scss

--- src/pages/SignupForm.js
+++ src/pages/SignupForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../App.css';
+import '../App.scss';
 import { useState } from "react";
 import { validationPassword, validationEmail } from '../utilities/inputValidation.js';
 import InputField from '../components/InputField';
